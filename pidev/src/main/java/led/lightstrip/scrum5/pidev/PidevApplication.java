231bf5ae7a9f55028b328ff76f17763c5ddc01de
MichaelAndrew22 <fiat.money@protonmail.com>
2023-09-30 23:31:45 -0700

    Basic Implementation and demonstration of the factory method. To design the led_strip classes need to locate led_strip methods, (which I can't locate in diozero), eyes needed on locating led_strip methods.

--- pidev/src/main/java/led/lightstrip/scrum5/pidev/PidevApplication.java
+++ pidev/src/main/java/led/lightstrip/scrum5/pidev/PidevApplication.java
@@ -1,13 +1,30 @@
 package led.lightstrip.scrum5.pidev;
 
+import models.Device;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PidevApplication {
 
+
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(PidevApplication.class, args);
+
+
+		// testing factory
+		Device myDevice = new factory().createDevice("neopixel");
+		myDevice.setDeviceName("DOOBEE");
+		myDevice.setGpioPin(18);
+		myDevice.setBrightness(22);
+		myDevice.setPixels(33);
+		System.out.println(myDevice.toString());
+
+
+
+
 	}
 
 }
