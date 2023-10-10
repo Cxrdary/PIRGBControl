19e82023c31b094bab71525ce29b039530ab9cbf
cxrdary <claycordary@gmail.com>
2023-10-10 16:01:01 -0700

    my working ui code

---
+++ pidev/src/main/java/led/lightstrip/scrum5/pidev/MVNWApp.java
@@ -0,0 +1,31 @@
+package led.lightstrip.scrum5.pidev;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import led.lightstrip.scrum5.pidev.controllers.MVNWController;
+import java.util.Objects;
+
+
+public class MVNWApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("mvnw.fxml")));
+
+        Scene scene = new Scene(root, 600, 400); // Set the preferred width and height
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("MVNW App");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+
+    }
+}
+
---
+++ pidev/src/main/java/led/lightstrip/scrum5/pidev/controllers/MVNWController.java
@@ -0,0 +1,56 @@
+package led.lightstrip.scrum5.pidev.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ColorPicker;
+
+public class MVNWController {
+
+    @FXML
+    private Button btn1;
+    @FXML
+    private Button btn2;
+    @FXML
+    private Button btn3;
+    @FXML
+    private ColorPicker cp1;
+    @FXML
+    private ColorPicker cp2;
+    @FXML
+    private ColorPicker cp3;
+
+
+
+    @FXML
+    public void initialize() {
+        // Initialize the ColorPickers with default colors
+        cp1.setValue(null);
+        cp2.setValue(null);
+        cp3.setValue(null);
+
+        // Initialize the Buttons with event handlers
+        btn1.setOnAction(event -> handleButton1Action());
+        btn2.setOnAction(event -> handleButton2Action());
+        btn3.setOnAction(event -> handleButton3Action());
+
+    }
+
+    @FXML
+    public void handleButton1Action() {
+        // Handle the action for btn1 here
+        System.out.println("Setting Color To Red");
+    }
+
+    @FXML
+    public void handleButton2Action() {
+        // Handle the action for btn2 here
+        System.out.println("Setting Color To Green");
+    }
+
+    @FXML
+    public void handleButton3Action() {
+        // Handle the action for btn3 here
+        System.out.println("Setting Color To Blue");
+    }
+}
+
